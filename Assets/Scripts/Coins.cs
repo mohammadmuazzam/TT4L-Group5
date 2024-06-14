a4d843490444e926e6e6a827d9ccee0e2749bc9f
Arefhasni <arefhasni005@gmail.com>
2024-06-15 02:47:32 +0800

    coins logic and moving

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -5,6 +5,35 @@ using UnityEngine;
 
 public class Coins : MonoBehaviour
 {
+    [SerializeField] float minY, maxY;
+    [SerializeField] float moveSpeed = 1.0f;
+
+    bool movingUp = true;
+
+    void Update()
+    {
+        if (movingUp && transform.position.y < maxY)
+        {
+            float newY = transform.position.y + moveSpeed * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+            if (transform.position.y >= maxY)
+            {
+                movingUp = false;
+            }
+        }
+
+        if (!movingUp && transform.position.y > minY)
+        {
+            float newY = transform.position.y - moveSpeed * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+            if (transform.position.y <= minY)
+            {
+                movingUp = true;
+            }
+        }
+    }
     public GameManager gameManager;
 
     // Update is called once per frame
